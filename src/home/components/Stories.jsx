061a1deb467688ed263f9846a4055cbc68ba53d6
Federico Passarelli <federico2797@gmail.com>
2023-04-19 14:34:19 -0300

    Finished home section v1.0

--- src/home/components/Stories.jsx
+++ src/home/components/Stories.jsx
@@ -1,4 +1,5 @@
 import { ArrowButtonWide } from "../../components/ArrowButtonWide";
+import { HorizontalColorfulRectangle } from "../../layout/components/HorizontalColorfulRectangle";
 
 const stories = [
     { image: 'bg-mountains', title: 'The Mountains', author: 'John Appleseed' },
@@ -12,12 +13,16 @@ export const Stories = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full">
         {
             stories.map( (story, index) => (
-                <article key={index} className={`relative flex flex-col w-full h-[375px] px-[33px] justify-end pb-10 ${story.image} bg-no-repeat bg-cover bg-center`}>
-                    <div className='image absolute w-full h-[375px] top-0 left-0 bg-gradient-to-b from-initial-grey to-end-grey opacity-100 z-10'></div>
-                    <h3 className="font-bold font-dm-sans text-lg text-white leading-[25px] pb-1 z-20">{story.title}</h3>
-                    <span className='font-dm-sans text-[13px] text-white z-20'>by {story.author}</span>
-                    <hr className='w-full bg-white opacity-25 mt-4 mb-5 z-20'/>
-                    <ArrowButtonWide extraStyle={'z-10'}>Read Story</ArrowButtonWide>
+                <article key={index} className="relative group lg:hover:translate-y-[-24px] duration-200">
+                    <div className={`relative flex flex-col w-full h-[375px] px-[33px] justify-end pb-10 ${story.image} bg-no-repeat bg-cover bg-center`}>
+                        <div className='image absolute w-full h-[375px] top-0 left-0 bg-gradient-to-b from-initial-grey to-end-grey opacity-100 z-10'></div>
+                        <h3 className="font-bold font-dm-sans text-lg text-white leading-[25px] pb-1 z-20">{story.title}</h3>
+                        <span className='font-dm-sans text-[13px] text-white z-20'>by {story.author}</span>
+                        <hr className='w-full bg-white opacity-25 mt-4 mb-5 z-20'/>
+                        <ArrowButtonWide extraStyle={'z-10'}>Read Story</ArrowButtonWide>
+                    </div>
+                    {/* Colorful rectangle on hover */}
+                    <div className="absolute bottom-[-6px] hidden group-hover:block h-[6px] w-full bg-gradient-to-tr from-[#FFC593] via-[#BC7198] to-[#5A77FF]"></div>
                 </article>
             ))
         }
