d564bb9dd3e623827f9c969ad959b308d5250e6b
Federico Passarelli <federico2797@gmail.com>
2023-04-24 19:15:40 -0300

    Added smooth scrolling

--- src/router/ScrollToTop.jsx
+++ src/router/ScrollToTop.jsx
@@ -1,11 +1,36 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+// export const ScrollToTop = () => {
+//     const { pathname } = useLocation();
+
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//     }, [pathname]);
+
+//     return null;
+// }
 export const ScrollToTop = () => {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        let animationFrameId;
+
+        const step = () => {
+        if (window.pageYOffset === 0) {
+            cancelAnimationFrame(animationFrameId);
+            return;
+        }
+
+        window.scroll(0, window.pageYOffset - 100);
+        animationFrameId = requestAnimationFrame(step);
+        };
+
+        animationFrameId = requestAnimationFrame(step);
+
+        return () => {
+        cancelAnimationFrame(animationFrameId);
+        };
     }, [pathname]);
 
     return null;
