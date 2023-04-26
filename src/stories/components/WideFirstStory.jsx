e7ce3ce80e5bb46fb34fae0f26ab2aaa92065d69
Federico Passarelli <federico2797@gmail.com>
2023-04-26 15:37:44 -0300

    Added photo for README

--- src/stories/components/WideFirstStory.jsx
+++ src/stories/components/WideFirstStory.jsx
@@ -4,7 +4,7 @@ export const WideFirstStory = () => {
   return (
     <div className='hidden md:flex flex-col w-full justify-center'>
         {/* Image of the article */}
-        <div className='w-full min-h-[650px] flex overflow-hidden bg-[url(stories/tablet/moon-of-appalacia.jpg)] bg-no-repeat bg-cover lg:bg-[url(stories/desktop/moon-of-appalacia.jpg)]'>
+        <div className="w-full min-h-[650px] flex overflow-hidden bg-[url('/stories/tablet/moon-of-appalacia.jpg')] bg-no-repeat bg-cover lg:bg-moon-of-appalacia">
             <div className="mx-[5%] text-white w-[50%] py-[48px] my-auto lg:mx-[7.77%] lg:max-w-[400px]">
                 <h4 className="mb-6 font-bold font-dm-sans text-xs tracking-[2px] uppercase">Last month’s featured story</h4>
                 <h1 className="mb-4 font-bold font-dm-sans text-[32px] tracking-[3.33px] uppercase md:text-[40px] md:leading-[48px] md:tracking-[4.17px]">Hazy full moon of appalachia</h1>
